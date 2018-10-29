7f0ece9611c25b7cfa3f9878e6797268b6af36dc
sandy <https://weichihsung@dev.azure.com>
2018-10-29 09:12:16 +0800

    networks

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+//var rpc_endpoint_kovan = "https://kovan.infura.io/v3/a0f96fa594774bad996d670e1ec06cb3";
+var rpc_endpoint_poa = "http://nodeone.centralus.cloudapp.azure.com:8503";
+var mnemonic = "taste public forest put voyage rabbit trial toward actress sudden letter attract";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +12,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    poa: {
+      provider: new HDWalletProvider(mnemonic, rpc_endpoint_poa),
+      gasPrice: '0x00',
+      port: 8503,
+      network_id: "1515"//1515
     }
   }
 };
