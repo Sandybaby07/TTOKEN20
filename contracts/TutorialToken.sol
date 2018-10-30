e32dea3e21c735d08c26f20a70f9fbe3fa5c9ff7
sandy <https://weichihsung@dev.azure.com>
2018-10-30 14:06:24 +0800

    ERC20.sol

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -1,15 +1,18 @@
 pragma solidity ^0.4.24;
 
-import "zeppelin-solidity/contracts/token/StandardToken.sol";
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
-contract TutorialToken is StandardToken {
+
+contract TutorialToken is ERC20 {
     string public name = "TutorialToken";
     string public symbol = "TT";
     uint8 public decimals = 2;
-    uint public INITIAL_SUPPLY = 12000;
-
+    uint256 public INITIAL_SUPPLY = 120000;
+    uint256 public TOTAL_SUPPLY;
+    mapping(address => uint256) public balanceOf;
+    
     constructor() public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
+        TOTAL_SUPPLY = INITIAL_SUPPLY;
+        balanceOf[msg.sender] = INITIAL_SUPPLY;
     }
 }
